0347f99b6497497f9dfd6906e7acec3b036bf972
Junaid Siddiqui <junaidas97@gmail.com>
2018-08-12 20:19:44 -0400

    Kiv Initial tests

---
+++ kivyApp.py
@@ -0,0 +1,12 @@
+import kivy
+import main
+kivy.require('1.0.6') # replace with your current kivy version !
+
+from kivy.app import App
+from kivy.uix.label import Label
+
+
+class MyApp(App):
+
+    def build(self):
+        main.run()
